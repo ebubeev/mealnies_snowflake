35acb1386d201706820dbd84bdf280eaf807acf7
ebubeev <eugene.bubeev@clarionhg.com>
2025-03-26 16:17:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop
+st.stop()
 
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
