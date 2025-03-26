3d778a44607daacaf5bd0cedeead4b5bb5a373fb
ebubeev <eugene.bubeev@clarionhg.com>
2025-03-26 16:04:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+rch_on# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -62,7 +62,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.') 
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     st.write(ingredients_string)
 
